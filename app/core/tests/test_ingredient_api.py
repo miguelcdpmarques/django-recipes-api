83ef1abce51e4ea65b1692fd8c40cfe9c679c59b
miguelcpmarques <miguelcdpmarques@gmail.com>
2020-04-25 20:17:44 +0100

    feat: added list ingredient feature

---
+++ app/core/tests/test_ingredient_api.py
@@ -0,0 +1,81 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Ingredient
+from recipe.serializers import IngredientSerializer
+
+
+INGREDIENTS_URL = reverse('recipe:ingredient-list')
+
+
+class PublicIngredientAPITest(TestCase):
+    """Tests ingredients API when not authenticated"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_unauthorized_user(self):
+        """Test that the login is required to use the api"""
+        res = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateIngredientsAPITest(TestCase):
+    """Test ingredients API with authenticated users"""
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            email='admin@admin.com',
+            password='testpass',
+            name='admin'
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_ingredients(self):
+        """Test retrieving ingredients"""
+        Ingredient.objects.create(user=self.user, name='Broccoli')
+        res = self.client.get(INGREDIENTS_URL)
+
+        ingredients = Ingredient.objects.all().order_by('-name')
+        serializer = IngredientSerializer(ingredients, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(serializer.data, res.data)
+    
+    def test_ingredients_limited_to_user(self):
+        """Test retrieving ingredients only from authenticated user"""
+        new_user = get_user_model().objects.create_user(
+            email='another@admin.com',
+            password='another',
+            name='another'
+        )
+        ingredient1 = Ingredient.objects.create(user=new_user, name='Salt')
+        ingredient2 = Ingredient.objects.create(user=self.user, name='Pepper')
+
+        res = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], ingredient2.name)
+
+    # def test_create_ingredient_successful(self):
+    #     """Test creating a new ingredient"""
+    #     payload = {'name': 'Carrot'}
+    #     res = self.client.post(INGREDIENTS_URL, payload)
+
+    #     exists = Ingredient.objects.filter(user=self.user, name=payload['name']).exists()
+
+    #     self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+    #     self.assertTrue(exists)
+    
+    # def test_create_ingredient_invalid(self):
+    #     """Test creating a new ingredient with invalid payload"""
+    #     payload = {}
+    #     res = self.client.post(INGREDIENTS_URL, payload)
+
+    #     self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -11,12 +11,11 @@ class TagSerializer(serializers.ModelSerializer):
         fields = ('id', 'name')
         read_only_fields = ('id',)
 
-    def create(self, validated_data):
-        """Create a new tag and return it"""
-        return Tag.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        """Update an existing tags instance and return it"""
-        instance.name = validated_data.get('name', instance.name)
-        instance.save()
-        return instance
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for the Ingredient Model"""
+
+    class Meta:
+        model = Ingredient
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
